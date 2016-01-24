4b2081facec90972e14fcac5fa64c3214cfd39b4
Jiahao Li <isundaylee.reg@gmail.com>
2016-01-24 22:28:00 +0800

    HTTP interface.

--- app.rb
+++ app.rb
@@ -2,11 +2,33 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'sinatra/base'
+require 'tempfile'
+require 'json'
 
 require 'code_runner'
 
 class CodeRunnerApp < Sinatra::Base
+  @@runner = CodeRunner.new
+
   post '/' do
-    ["Hello, world! "]
+    content_type :json
+
+    if params['code'].nil? || params['code'][:filename].nil?
+      status 400
+      body ({success: false, error: "Missing `code' parameter. "}).to_json
+      return
+    end
+
+    begin
+      result = @@runner.run(params['code'][:tempfile].path)
+
+      status 200
+      body result.merge({success: true}).to_json
+      return
+    rescue CodeRunner::NoCompatibleRunner => e
+      status 400
+      body ({success: false, error: "File type not supported yet. "}).to_json
+      return
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,47 @@
 require_relative './spec_helper'
 
+require 'json'
+require 'tempfile'
+
 describe CodeRunnerApp do
   describe "POST /" do
-    it "should return `Hello, world! '" do
+    it "should report missing `code' parameter" do
       post '/'
-      expect(last_response).to be_ok
-      expect(last_response.body).to eq('Hello, world! ')
+
+      json_response = JSON.parse(last_response.body)
+
+      expect(last_response.status).to eq(400)
+      expect(json_response['success']).to eq(false)
+      expect(json_response['error']).to eq("Missing `code' parameter. ")
+    end
+
+    it "should give correct outputs and status code" do
+      @tmpfile = Tempfile.new(['test', '.rb'])
+      @tmpfile.write("puts 'Hello, world! '; STDERR.puts 'error'; exit(3)")
+      @tmpfile.close
+
+      post '/', code: Rack::Test::UploadedFile.new(@tmpfile.path)
+
+      json_response = JSON.parse(last_response.body)
+
+      expect(last_response.status).to eq(200)
+      expect(json_response['success']).to eq(true)
+      expect(json_response['stdout']).to eq("Hello, world! \n")
+      expect(json_response['stderr']).to eq("error\n")
+      expect(json_response['status']).to eq(3)
+    end
+
+    it "should report unsupported file types" do
+      @tmpfile = Tempfile.new(['test', '.something_weird'])
+      @tmpfile.close
+
+      post '/', code: Rack::Test::UploadedFile.new(@tmpfile.path)
+
+      json_response = JSON.parse(last_response.body)
+
+      expect(last_response.status).to eq(400)
+      expect(json_response['success']).to eq(false)
+      expect(json_response['error']).to eq("File type not supported yet. ")
     end
   end
 end
